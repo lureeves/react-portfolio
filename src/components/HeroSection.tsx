fa8599bfa72eee57922a697bdfe9124ed5ec6673
Lukas Reeves <lukasreeves@Lukass-MacBook-Air.local>
2023-10-03 18:09:42 -0700

    removed boilerplate, added components and public folder

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class HeroSection extends Component {
+  render() {
+    return (
+      <div>HeroSection</div>
+    )
+  }
+}
+
+export default HeroSection
---
+++ src/components/ProjectsSection.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class ProjectsSection extends Component {
+  render() {
+    return (
+      <div>ProjectsSection</div>
+    )
+  }
+}
+
+export default ProjectsSection
